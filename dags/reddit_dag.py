f2d3560187dd90b1add11bd79cebe3e17e591072
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-04-07 13:26:18 +0200

    docker compose works

---
+++ dags/reddit_dag.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+import os 
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+default_args = {
+    "owner": "abdelrahman",
+    "start_date": datetime(2025, 4, 7),
+}
+
+file_postfix = datetime.now().strftime("%Y%m%d")
+
+dag = DAG(
+    dag_id="etl_reddit_pipeline",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+    tag=["reddit", "etl"]
+)
+
+
+# Extraction from reddit
+extract = PythonOperator(
+    task_id="reddit_extraction",
+    python_callable=reddit_pipeline,
+    op_kwargs={
+        'file_name': f'reddit_{file_postfix}',
+        'subreddit': 'dataengineering',
+        'time_filter': 'day',
+        'limit': 100
+    }
+)
---
+++ pipelines/reddit_pipeline.py
@@ -0,0 +1,6 @@
+def reddit_pipeline(file_name, subreddit, time_filter="day", limit=None):
+    # Connecting to reddit instance 
+    instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
+    # Extraction
+    # Transformation 
+    # Loading
