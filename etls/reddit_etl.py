95dab3edaf16c03b09971e36c42fd1f38f549112
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-04-26 23:53:22 +0300

    transform basics

--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -1,6 +1,9 @@
 from praw import Reddit # type: ignore
 from utils.constants import POST_FIELDS
 import sys
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+
 
 def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
     try:
@@ -28,3 +31,13 @@ def extract_post(reddit_instance, subreddit, time_filter, limit=None) -> None:
         post_list.append(post)
     
     return post_list
+
+def transform_data(post_df: pd.DataFrame):
+    post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
+    post_df['over_18'] = np.where((post_df['over_18'] == True), True, False)
+    post_df['author'] = post_df['author'].astype(str)
+    
+    return post_df
+
+def load_data_to_csv(data: pd.DataFrame, path: str):
+    data.to_csv(path, index=False)
--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -1,10 +1,17 @@
-from utils.constants import CLIENT_ID, SECRET
-from etls.reddit_etl import connect_reddit, extract_post
+from utils.constants import CLIENT_ID, SECRET, OUTPUT_PATH
+from etls.reddit_etl import connect_reddit, extract_post, transform_data, load_data_to_csv
+import pandas as pd 
 
 def reddit_pipeline(file_name, subreddit, time_filter="day", limit=None):
     # Connecting to reddit instance 
     instance = connect_reddit(CLIENT_ID, SECRET, 'notabdelrahman')
     # Extraction
     posts = extract_post(instance, subreddit, time_filter, limit)
+    posts_df = pd.DataFrame(posts)
     # Transformation 
-    # Loading
+    post_df = transform_data(posts_df)
+    # Loading to csv
+    file_path = f'{OUTPUT_PATH}/{file_name}.csv'
+    load_data_to_csv(post_df, file_path)
+
+    return file_path
